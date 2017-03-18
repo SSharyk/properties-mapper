1c52b7284f6658a3790b819c69bfcf5d60bf933f
SSharyk <ssharyk@gmail.com>
2017-03-18 18:14:38 +0400

    * Fixed bug with arrays * Simple optimization

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
  * source {string} - Comma-separated list of properties the object has
  * target {string} - Comma-separated list of properties the new object has have
  * isRecurcive {boolean} - True if nested objects should be converted too
+ * @returns {JSON Object} New objwtc with renamed properties
  */
 function map(obj, source, target, isRecursive = true) {
 	// Check if parameters have invalid types
@@ -21,17 +22,25 @@ function map(obj, source, target, isRecursive = true) {
  
  	// each property is checked ...
 	for (let propertyName in obj) {
-		// take into account that JSON object can have nested objects
-		if (isRecursive && ((typeof obj[propertyName]) == "object"))
-			obj[propertyName] = map(obj[propertyName], source, target, isRecursive);
-
 		// ... if need be converted or not
 		let propIndexInSource = propS.indexOf(propertyName);
-		if (propIndexInSource != -1) {
-			res[propT[propIndexInSource]] = obj[propertyName];
-		} else {
-			res[propertyName] = obj[propertyName];
+		let newName = (propIndexInSource != -1) ? propT[propIndexInSource] : propertyName;
+
+		// take into account that property could be an array
+		if (isRecursive && obj[propertyName] instanceof Array) {
+			res[newName] = [];
+			for (var i = 0; i < obj[propertyName].length; i++) {
+				let mappedItem = map(obj[propertyName][i], source, target, isRecursive)
+				res[newName].push(mappedItem);
+			}
+			continue;
 		}
+
+		// take into account that JSON object can have nested objects
+		if (isRecursive && ((typeof obj[propertyName]) == "object"))
+			res[newName] = map(obj[propertyName], source, target, isRecursive);
+		else
+			res[newName] = obj[propertyName];
 	}
 
 	return res;
